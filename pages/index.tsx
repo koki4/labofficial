9d549dc4f57e04cbeb9ec89fb4cfdb2711d335b2
koki4 <koki.estark@gmail.com>
2024-03-12 16:57:11 +0900

    second

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,15 @@ import Article1 from "./components/Article1"
 import Article2 from "./components/Article2";
 import ResponsiveNavbar from "./components/ResponsiveNavbar";
 
+// 例として、外部APIからデータをフェッチする場合
+export async function getServerSideProps() {
+  // 外部APIからデータをフェッチするロジック
+  // const data = await fetchData();
+
+  // フェッチしたデータをpropsとしてページコンポーネントに渡す
+  return { props: { /* data */ } };
+}
+
 export default function Home() {
     return (
         <div>
